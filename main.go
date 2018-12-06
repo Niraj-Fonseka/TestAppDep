28d3da71b48c98ce3ee245bdb139db52b0afc02e
Niraj-Fonseka <fonseka.live@utexas.edu>
2018-12-06 08:29:55 -0600

    Adding dep

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+import (
+        "fmt"
+        "github.com/Pallinder/go-randomdata"
+)
+func main(){
+        fmt.Println("Running the TestApp")
+        fmt.Println(randomdata.SillyName())
+}
